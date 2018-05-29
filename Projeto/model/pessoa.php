ca1a78e84a28d2e7bb3d563635ad20468601e2f8
Heuller <heullergit@gmail.com>
2018-05-29 12:23:52 -0300

    pastinha

---
+++ Projeto/model/pessoa.php
@@ -0,0 +1,22 @@
+<?php
+
+$valor = "irineu";
+
+echo $valor;
+
+/*
+class Pessoa{
+
+    private $nome;
+    private $nascimento;
+    private $cpf;
+    private $telefone;
+    private $email;
+    private $cidade;
+    private $bairro;
+    private $endereco;
+    private $celular;
+
+}
+*/
+?>
